86edd44a9d3e9ed9d36e17e5407c2e3a0a40e967
remyCases <remy.cases@gmail.com>
2025-01-13 16:59:05 +0100

    [Minor] adds hashmap with string keys

--- include/utils.h
+++ include/utils.h
@@ -14,6 +14,7 @@ typedef uint32_t hash_t;
 
 #define HASHMAP_ELMT_TYPE(K, V) SS_CAT_UND(hmel, K, V, t)
 #define HASHMAP_ELMT_TYPE_PTR(K, V) SS_CAT_UND(hmel, K, V, ptr_t)
+#define HASHMAP_ELMT_TYPE_STR(V) SS_CAT_UND(hmel, str, V, t)
 #define HASHMAP_ELMT(K, V)                      \
 typedef struct SS_CAT_UND(hmel, K, V, s) {      \
     V value;                                    \
@@ -28,8 +29,16 @@ typedef struct SS_CAT_UND(hmel, K, V, ptr_s) {  \
     hash_t hash;                                \
 } HASHMAP_ELMT_TYPE_PTR(K, V);
 
+#define HASHMAP_ELMT_STR(V)                     \
+typedef struct SS_CAT_UND(hmel, str, V, s) {    \
+    V value;                                    \
+    char* key;                                  \
+    hash_t hash;                                \
+} HASHMAP_ELMT_TYPE_STR(V);
+
 #define HASHMAP_TYPE(K, V) SS_CAT_UND(hm, K, V, t)
 #define HASHMAP_TYPE_PTR(K, V) SS_CAT_UND(hm, K, V, ptr_t)
+#define HASHMAP_TYPE_STR(V) SS_CAT_UND(hm, str, V, t)
 #define HASHMAP(K, V)                           \
 typedef struct SS_CAT_UND(hm, K, V, s) {        \
     int32_t current_size;                       \
@@ -47,6 +56,15 @@ typedef struct SS_CAT_UND(hm, K, V, ptr_s) {    \
     int32_t grow_threshold;                     \
     HASHMAP_ELMT_TYPE_PTR(K, V)* elements;      \
 } HASHMAP_TYPE_PTR(K, V);
+
+#define HASHMAP_STR(V)                          \
+typedef struct SS_CAT_UND(hm, str, V, s) {      \
+    int32_t current_size;                       \
+    int32_t used_count;                         \
+    int32_t current_mask;                       \
+    int32_t grow_threshold;                     \
+    HASHMAP_ELMT_TYPE_STR(V)* elements;         \
+} HASHMAP_TYPE_STR(V);
 
 #define GET_CONTAINER_N(K, V) S_CAT_UND(get_container, K, V)
 #define GET_CONTAINER(K, V)                                                                         \
@@ -79,6 +97,21 @@ ERROR_MLS GET_CONTAINER_N(K, V)(HASHMAP_TYPE_PTR(K, V)* hashmap, K key, HASHMAP_
     return MSL_OBJECT_NOT_IN_LIST;                                                                          \
 }
 
+#define GET_CONTAINER_STR(V)                                                                                \
+ERROR_MLS GET_CONTAINER_N(str, V)(HASHMAP_TYPE_STR(V)* hashmap, char* key, HASHMAP_ELMT_TYPE_STR(V)* value) \
+{                                                                                                           \
+    hash_t value_hash = hash_key_char(key);                                                                 \
+    int32_t ideal_position = (int)(value_hash & hashmap->current_mask);                                     \
+    for (HASHMAP_ELMT_TYPE_STR(V) current_element = hashmap->elements[ideal_position];                      \
+        current_element.hash != 0;                                                                          \
+        current_element = hashmap->elements[(++ideal_position) & hashmap->current_mask]) {                  \
+        if (current_element.key != key) continue;                                                           \
+        *value = current_element;                                                                           \
+        return MSL_SUCCESS;                                                                                 \
+    }                                                                                                       \
+    return MSL_OBJECT_NOT_IN_LIST;                                                                          \
+}
+
 #define GET_VALUE_N(K, V) S_CAT_UND(get_value, K, V)
 #define GET_VALUE(K, V)                                                                     \
 ERROR_MLS GET_VALUE_N(K, V)(HASHMAP_TYPE(K, V)* hashmap, K key, V* value)                   \
@@ -100,6 +133,16 @@ ERROR_MLS GET_VALUE_N(K, V)(HASHMAP_TYPE_PTR(K, V)* hashmap, K key, V** value)  
     return MSL_SUCCESS;                                                                     \
 }
 
+#define GET_VALUE_STR(V)                                                                    \
+ERROR_MLS GET_VALUE_N(str, V)(HASHMAP_TYPE_STR(V)* hashmap, char* key, V* value)            \
+{                                                                                           \
+    HASHMAP_ELMT_TYPE_STR(V)* object_container = NULL;                                      \
+    if (GET_CONTAINER_N(str, V)(hashmap, key, object_container) == MSL_OBJECT_NOT_IN_LIST)  \
+        return MSL_OBJECT_NOT_IN_LIST;                                                      \
+    *value = object_container->value;                                                       \
+    return MSL_SUCCESS;                                                                     \
+}
+
 #define HASH(K, V)      \
     HASHMAP_ELMT(K, V)  \
     HASHMAP(K, V)       \
@@ -115,6 +158,14 @@ ERROR_MLS GET_VALUE_N(K, V)(HASHMAP_TYPE_PTR(K, V)* hashmap, K key, V** value)  
 #define FUNC_HASH_PTR(K, V) \
     GET_CONTAINER_PTR(K, V) \
     GET_VALUE_PTR(K, V)
+
+#define HASH_STR(V)      \
+    HASHMAP_ELMT_STR(V)  \
+    HASHMAP_STR(V)
+
+#define FUNC_HASH_STR(V) \
+    GET_CONTAINER_STR(V) \
+    GET_VALUE_STR(V) 
 
 #define LINKEDLIST_TYPE(T) S_CAT_UND(ll, T, t)
 #define LINKEDLIST(T)                   \
--- include/utils_macro.h
+++ include/utils_macro.h
@@ -13,11 +13,11 @@
 #define S_CAT_UND(A, B, C)              CAT_UND(CAT_UND(A, B), C)
 #define SS_CAT_UND(A, B, C, D)          CAT_UND(S_CAT_UND(A, B, C), D)
 
-#define TO_ENUM(A, ...)                 A,
-#define TO_CASE_STR(A, ...)             case A: return STR(A);
-#define TO_CASE_PRINT(A, F, L, ...)     CAT_UND(TO_CASE_PRINT, __VA_ARGS__)(A, F, L)
-#define TO_CASE_PRINT_SUCCESS(A, F, L)  case A: break;
-#define TO_CASE_PRINT_(A, F, L)         case A: printf("[!] Line %d, in function %s: "STR(A)" (code error %d)\n", L, F, A); break;
+#define TO_ENUM(A, ...)                     A,
+#define TO_CASE_STR(A, ...)                 case A: return STR(A);
+#define TO_CASE_PRINT(A, F, NF, NL, ...)    CAT_UND(TO_CASE_PRINT, __VA_ARGS__)(A, F, NF, NL)
+#define TO_CASE_PRINT_SUCCESS(A, F, NF, NL) case A: break;
+#define TO_CASE_PRINT_(A, F, NF, NL)        case A: printf("[!] File %s line %d, in function %s: "STR(A)" (code error %d)\n", NF, NL, F, A); break;
 
 #define COMPILE_TIME_ASSERT(e)          COMPILE_TIME_ASSERT_(e)
 #define COMPILE_TIME_ASSERT_(e)         typedef char assertion_##__LINE__[(e) ? 1 : -1]
