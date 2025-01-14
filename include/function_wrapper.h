4daa2370c90367d3c9a944f0196ff061e4dc6947
remyCases <remy.cases@gmail.com>
2025-01-14 03:48:36 +0100

    [Major] ends function wrapper abstraction struct

--- include/function_wrapper.h
+++ include/function_wrapper.h
@@ -7,6 +7,7 @@
 
 #include "dxgi.h"
 #include "gml_structs.h"
+#include <stdbool.h>
 
 #define CALL(F, T)										\
 T Call(F* f) {											\
@@ -22,18 +23,40 @@ T Call(F* f, __VA_ARGS__) {								\
 	return f->result;									\
 }
 
-#define FUNCTION_WRAPPER(F, T, ...) 							\
-typedef struct SS_CAT_UND(fn, T, ARG_LENGTH(__VA_ARGS__), s)	\
-{																\
-	typedef struct args_s {										\
-		/*TODO: implement that*/								\
-	} args_t; 													\
-	args_t args;												\
-	T (*function)(args_t);										\
-	bool called_original;										\
-	T result;													\
-	T (*Call)(); 												\
-	T (*Call)(__VA_ARGS__);										\
+#define EXPANDS(N, ...) S_CAT(EXPAND_, N)(__VA_ARGS__)
+#define EXPAND_0(...)
+#define EXPAND_1(a0, ...) a0
+#define EXPAND_2(a0, ...) a0 EXPAND_1(__VA_ARGS__)
+#define EXPAND_3(a0, ...) a0 EXPAND_2(__VA_ARGS__)
+#define EXPAND_4(a0, ...) a0 EXPAND_3(__VA_ARGS__)
+#define EXPAND_5(a0, ...) a0 EXPAND_4(__VA_ARGS__)
+#define EXPAND_6(a0, ...) a0 EXPAND_5(__VA_ARGS__)
+
+#define TO_ARGS(N, ...) GET_ARG_N(N, __VA_ARGS__) S_CAT(_, N);
+
+#define GET_ARG_N(N, ...) GET_ARG_N_(N, __VA_ARGS__)
+#define GET_ARG_N_(N, ...) S_CAT(GET_ARG_, N)(__VA_ARGS__)
+
+#define GET_ARG_0(a0, ...) a0
+#define GET_ARG_1(a0, a1, ...) a1
+#define GET_ARG_2(a0, a1, a2, ...) a2
+#define GET_ARG_3(a0, a1, a2, a3, ...) a3
+#define GET_ARG_4(a0, a1, a2, a3, a4, ...) a4
+#define GET_ARG_5(a0, a1, a2, a3, a4, a5, ...) a5
+#define GET_ARG_6(a0, a1, a2, a3, a4, a5, a6, ...) a6
+
+#define FUNCTION_WRAPPER(F, T, ...) 								\
+typedef struct SS_CAT_UND(arg, T, ARG_LENGTH(__VA_ARGS__), s) {		\
+	EXPANDS(ARG_LENGTH(__VA_ARGS__), ARG_COUNT(TO_ARGS, __VA_ARGS__))\
+} SS_CAT_UND(arg, T, ARG_LENGTH(__VA_ARGS__), t);					\
+typedef struct SS_CAT_UND(fn, T, ARG_LENGTH(__VA_ARGS__), s)		\
+{																	\
+	SS_CAT_UND(arg, T, ARG_LENGTH(__VA_ARGS__), t) args;			\
+	T (*function)(SS_CAT_UND(arg, T, ARG_LENGTH(__VA_ARGS__), t));	\
+	bool called_original;											\
+	T result;														\
+	T (*Call)(); 													\
+	T (*CallA)(__VA_ARGS__);										\
 } F;
 
 FUNCTION_WRAPPER(FWCodeEvent, bool, instance_t*, instance_t*, code_t*, int, rvalue_t*)
@@ -41,5 +64,4 @@ FUNCTION_WRAPPER(FWFrame, HRESULT, IDXGISwapChain*, UINT, UINT)
 FUNCTION_WRAPPER(FWResize, HRESULT, IDXGISwapChain*, UINT, UINT, UINT, DXGI_FORMAT, UINT)
 FUNCTION_WRAPPER(FWWndProc, LRESULT, HWND, UINT, WPARAM, LPARAM)
 CALL(FWWndProc, LRESULT)
-ARG_LENGTH_(instance_t*)
 #endif  /* !FUNCTION_WRAPPER_H_ */
--- include/utils_macro.h
+++ include/utils_macro.h
@@ -19,17 +19,29 @@
 #define TO_CASE_PRINT_SUCCESS(A, F, NF, NL) case A: break;
 #define TO_CASE_PRINT_(A, F, NF, NL)        case A: printf("[!] File %s line %d, in function %s: "STR(A)" (code error %d)\n", NF, NL, F, A); break;
 
-# define ARG_LENGTH(...) ARG_LENGTH__(__VA_ARGS__)
-# define ARG_LENGTH__(...) ARG_LENGTH_(,##__VA_ARGS__,                         \
-    63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45,\
-    44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26,\
-    25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6,\
+#define ARG_LENGTH(...) ARG_LENGTH__(__VA_ARGS__)
+#define ARG_LENGTH__(...) ARG_LENGTH_(,##__VA_ARGS__,                           \
+    63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, \
+    44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, \
+    25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, \
     5, 4, 3, 2, 1, 0)
-# define ARG_LENGTH_(_, _63, _62, _61, _60, _59, _58, _57, _56, _55, _54, _53, \
-    _52, _51, _50, _49, _48, _47, _46, _45, _44, _43, _42, _41, _40, _39, _38, \
-    _37, _36, _35, _34, _33, _32, _31, _30, _29, _28, _27, _26, _25, _24, _23, \
-    _22, _21, _20, _19, _18, _17, _16, _15, _14, _13, _12, _11, _10, _9, _8,   \
+#define ARG_LENGTH_(_, _63, _62, _61, _60, _59, _58, _57, _56, _55, _54, _53,   \
+    _52, _51, _50, _49, _48, _47, _46, _45, _44, _43, _42, _41, _40, _39, _38,  \
+    _37, _36, _35, _34, _33, _32, _31, _30, _29, _28, _27, _26, _25, _24, _23,  \
+    _22, _21, _20, _19, _18, _17, _16, _15, _14, _13, _12, _11, _10, _9, _8,    \
     _7, _6, _5, _4, _3, _2, _1, Count, ...) Count
+
+#define ARG_COUNT(X, ...) ARG_COUNT__(X, __VA_ARGS__)
+#define ARG_COUNT__(X, ...) ARG_COUNT_(,##__VA_ARGS__,                          \
+    X(63, __VA_ARGS__), X(62, __VA_ARGS__), X(61, __VA_ARGS__), X(60, __VA_ARGS__), X(59, __VA_ARGS__), X(58, __VA_ARGS__), X(57, __VA_ARGS__), X(56, __VA_ARGS__), X(55, __VA_ARGS__), X(54, __VA_ARGS__), X(53, __VA_ARGS__), X(52, __VA_ARGS__), X(51, __VA_ARGS__), X(50, __VA_ARGS__), X(49, __VA_ARGS__), X(48, __VA_ARGS__), X(47, __VA_ARGS__), X(46, __VA_ARGS__), X(45, __VA_ARGS__), \
+    X(44, __VA_ARGS__), X(43, __VA_ARGS__), X(42, __VA_ARGS__), X(41, __VA_ARGS__), X(40, __VA_ARGS__), X(39, __VA_ARGS__), X(38, __VA_ARGS__), X(37, __VA_ARGS__), X(36, __VA_ARGS__), X(35, __VA_ARGS__), X(34, __VA_ARGS__), X(33, __VA_ARGS__), X(32, __VA_ARGS__), X(31, __VA_ARGS__), X(30, __VA_ARGS__), X(29, __VA_ARGS__), X(28, __VA_ARGS__), X(27, __VA_ARGS__), X(26, __VA_ARGS__), \
+    X(25, __VA_ARGS__), X(24, __VA_ARGS__), X(23, __VA_ARGS__), X(22, __VA_ARGS__), X(21, __VA_ARGS__), X(20, __VA_ARGS__), X(19, __VA_ARGS__), X(18, __VA_ARGS__), X(17, __VA_ARGS__), X(16, __VA_ARGS__), X(15, __VA_ARGS__), X(14, __VA_ARGS__), X(13, __VA_ARGS__), X(12, __VA_ARGS__), X(11, __VA_ARGS__), X(10, __VA_ARGS__), X(9, __VA_ARGS__), X(8, __VA_ARGS__), X(7, __VA_ARGS__), X(6, __VA_ARGS__), \
+    X(5, __VA_ARGS__), X(4, __VA_ARGS__), X(3, __VA_ARGS__), X(2, __VA_ARGS__), X(1, __VA_ARGS__), X(0, __VA_ARGS__))
+#define ARG_COUNT_(_, _63, _62, _61, _60, _59, _58, _57, _56, _55, _54, _53,    \
+    _52, _51, _50, _49, _48, _47, _46, _45, _44, _43, _42, _41, _40, _39, _38,  \
+    _37, _36, _35, _34, _33, _32, _31, _30, _29, _28, _27, _26, _25, _24, _23,  \
+    _22, _21, _20, _19, _18, _17, _16, _15, _14, _13, _12, _11, _10, _9, _8,    \
+    _7, _6, _5, _4, _3, _2, _1, Count, ...) __VA_ARGS__
 
 #define COMPILE_TIME_ASSERT(e)          COMPILE_TIME_ASSERT_(e)
 #define COMPILE_TIME_ASSERT_(e)         typedef char assertion_##__LINE__[(e) ? 1 : -1]
